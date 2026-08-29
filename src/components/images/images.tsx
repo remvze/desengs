25ef8115bc0eb99f3a88890ef2bb96bf39d3d073
MAZE <remvze@proton.me>
2026-08-29 13:01:26 +0330

    feat: work on seo

--- src/components/images/images.tsx
+++ src/components/images/images.tsx
@@ -1,4 +1,4 @@
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Masonry from 'react-masonry-css';
 
 import { images } from '@/data/images';
 
@@ -7,42 +7,37 @@ import styles from './images.module.css';
 export function Images() {
   return (
     <section className={styles.images}>
-      <ResponsiveMasonry
-        columnsCountBreakPoints={{ 1000: 3, 350: 1, 600: 2 }}
-        gutterBreakPoints={{ 0: '20px' }}
+      <Masonry
+        breakpointCols={{ 550: 1, 900: 2, default: 3 }}
+        className={styles.masonry}
+        columnClassName={styles.column}
       >
-        <Masonry>
-          {images.map(image => (
-            <div className={styles.og} key={image.slug}>
-              <a
-                className={styles.image}
-                href={`/inspiration/og/${image.slug}`}
-              >
-                <img
-                  alt={`OG of ${image.source.name}`}
-                  loading="lazy"
-                  src={image.image}
-                  style={{
-                    aspectRatio: `${image.dimensions.width} / ${image.dimensions.height}`,
-                  }}
-                />
-              </a>
+        {images.map(image => (
+          <div className={styles.og} key={image.slug}>
+            <a className={styles.image} href={`/inspiration/og/${image.slug}`}>
+              <img
+                alt={`OG of ${image.source.name}`}
+                height={image.dimensions.height}
+                loading="lazy"
+                src={image.image}
+                width={image.dimensions.width}
+              />
+            </a>
 
-              <div className={styles.details}>
-                <p>{image.source.name}</p>
-                <a
-                  className={styles.link}
-                  href={image.source.url}
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  {image.source.address}
-                </a>
-              </div>
+            <div className={styles.details}>
+              <p>{image.source.name}</p>
+              <a
+                className={styles.link}
+                href={image.source.url}
+                rel="noreferrer"
+                target="_blank"
+              >
+                {image.source.address}
+              </a>
             </div>
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+          </div>
+        ))}
+      </Masonry>
     </section>
   );
 }
